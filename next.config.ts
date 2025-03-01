054679fa7006368d3d3183476efaecbb439efd48
Anthony Castellano <tonycastellano13@gmail.com>
2025-03-01 09:01:13 -0500

    change base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/castellano-coding',
+  assetPrefix: '/castellano-coding',
 };
 
 export default nextConfig;
