f6f5d1cbf9b37e8e11725127456d67763a02e180
Anthony Castellano <tonycastellano13@gmail.com>
2025-03-02 17:41:57 -0500

    add google form

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -3,7 +3,17 @@ export default function SignUp() {
 
     return (
         <div>
-            Sign up
+            
+            <div className="flex flex-col items-center justify-center mx-8 my-6">
+                <iframe
+                    src="https://docs.google.com/forms/d/e/1FAIpQLSeo7bzBDpO3BJwXsTP3l0qJCrVt9LFZ_y-pn5GKJ8B6-1RxyA/viewform?embedded=true"
+                    className="h-screen w-screen overflow-hidden overflow-y-hidden"
+                    frameBorder="0"
+                    marginHeight={0}
+                    marginWidth={0}>
+                    Loading...
+                </iframe>
+            </div>
         </div>
     );
 }
